7180ae3deb5d88667b78dab82c38045f341c5a2d
owenmather <owenmather7@gmail.com>
2016-04-27 20:56:11 +0100

    test

--- app/scripts/controllers/newGameController.js
+++ app/scripts/controllers/newGameController.js
@@ -7,19 +7,28 @@
     
     function newGameController($scope){
        
-        $scope.runThis= function() {
-        console.log("it ran");
-        var stage = new createjs.Stage("demoCanvas");  
-        var circle = new createjs.Shape();
-        circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-        circle.x = 100;
-        circle.y = 100;
-        stage.addChild(circle);
-        stage.update();
+        $scope.runThis= function() {//this is newGameCanvas
+            
+            var canvas =document.getElementById("demoCanvas");
+            canvas.style.backgroundColor = 'rgba(0, 255, 127, 0.8)';
+            var stage = new createjs.Stage("demoCanvas");  
+            var text = new createjs.Text("Memory", "bold 40px Lato", "#f8f3f5");
+           
+            text=centerThis(canvas,text);
+   
+          
+            stage.addChild(text);
+            stage.update();
     }
         
     }//end Game Contoleer
     
-    
+    //takes canvas and object and returns object with position centered on canvas
+    function centerThis(canvas, myObject){ 
+        var b =myObject.getBounds();  
+        myObject.x = canvas.width/2 - b.width/2; 
+        myObject.y = canvas.height/2 - b.height/2;
+        return myObject;
+    }
     
 })();
