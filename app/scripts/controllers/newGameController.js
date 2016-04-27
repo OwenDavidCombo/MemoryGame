782cdbe4fad8bd187a079656f3ed2d007209393f
owenmather <owenmather7@gmail.com>
2016-04-27 23:50:45 +0100

    s

--- app/scripts/controllers/newGameController.js
+++ app/scripts/controllers/newGameController.js
@@ -42,29 +42,29 @@
                 var cornercards = new createjs.Bitmap(img);
                     cornercards.x=-2;
                     cornercards.y=20;
-                    cornercards.scaleX=0.1;
-                    cornercards.scaleY=0.1;
+                    cornercards.scaleX=0.5;
+                    cornercards.scaleY=0.5;
                     cornercards.rotation= -30;
 
                 var cornercards2 = new createjs.Bitmap(img);
                     cornercards2.x=canvas.width-46;
                     cornercards2.y=-10;
-                    cornercards2.scaleX=0.1;
-                    cornercards2.scaleY=0.1;
+                    cornercards2.scaleX=0.5;
+                    cornercards2.scaleY=0.5;
                     cornercards2.rotation= 30;
 
                 var cornercards3 = new createjs.Bitmap(img);
                     cornercards3.x=50;
                     cornercards3.y=canvas.height+10;
-                    cornercards3.scaleX=0.1;
-                    cornercards3.scaleY=0.1;
+                    cornercards3.scaleX=0.5;
+                    cornercards3.scaleY=0.5;
                     cornercards3.rotation= -150;
 
                 var cornercards4 = new createjs.Bitmap(img);
                     cornercards4.x=canvas.width+8;
                     cornercards4.y=canvas.height-20;
-                    cornercards4.scaleX=0.1;
-                    cornercards4.scaleY=0.1;
+                    cornercards4.scaleX=0.5;
+                    cornercards4.scaleY=0.5;
                     cornercards4.rotation= 150;
                 
                 stage.addChild(cornercards,cornercards2,cornercards3,cornercards4);
@@ -83,6 +83,11 @@
             text2 = centerThis(canvas,text2);
             text2.y = text2.y+75;
             stage.enableMouseOver(10);
+            
+            textContainer = new createjs.Container();
+            text.shadow=new createjs.Shadow("#000000", 5, 5, 10);
+            textContainer.addChild(text);
+            textContainer.shadow=new createjs.Shadow("#000000", 5, 5, 10);
             buttonContainer = new createjs.Container();
             buttonContainer.addChild(shape,shape2,text2);
             buttonContainer.cursor = "pointer";
@@ -118,7 +123,7 @@
             }
 
             
-            stage.addChild(text,buttonContainer);
+            stage.addChild(textContainer,buttonContainer);
             stage.enableMouseOver();
             stage.update();
     }
