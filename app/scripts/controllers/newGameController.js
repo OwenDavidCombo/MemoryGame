70daaff073d97cd5aec497f26b6492a9c5688012
David Mather <davidmather@live.ie>
2016-04-27 22:31:49 +0100

    Image
    Added a yeoman image on start click

--- app/scripts/controllers/newGameController.js
+++ app/scripts/controllers/newGameController.js
@@ -13,11 +13,11 @@
            
              var ctx = document.createElement("canvas").getContext("2d"),
             dpr = window.devicePixelRatio || 1,
-        bsr = ctx.webkitBackingStorePixelRatio ||
-              ctx.mozBackingStorePixelRatio ||
-              ctx.msBackingStorePixelRatio ||
-              ctx.oBackingStorePixelRatio ||
-              ctx.backingStorePixelRatio || 1;
+            bsr = ctx.webkitBackingStorePixelRatio ||
+                  ctx.mozBackingStorePixelRatio ||
+                  ctx.msBackingStorePixelRatio ||
+                  ctx.oBackingStorePixelRatio ||
+                  ctx.backingStorePixelRatio || 1;
             
             ratio = dpr / bsr;
               canvas.width = 800 * ratio;
@@ -28,36 +28,57 @@
 
             context =canvas.getContext("2d");
             canvas.style.backgroundColor = 'rgba(0, 255, 127, 0.8)';
-            var stage = new createjs.Stage("demoCanvas");  
-            var text = new createjs.Text("Memory", "bold 120px Lato", "#f8f3f5");
-            var text2 = new createjs.Text("Start", "bold 39px Lato", "#000000");           
+            var stage = new createjs.Stage("demoCanvas");
             
-            var graphics = new createjs.Graphics().beginFill("#000000").drawRect(canvas.width/2 -50, canvas.height/2+60, 100, 30);
+            var text = new createjs.Text("Memory", "bold 120px Lato", "#f8f3f5");
+            var text2 = new createjs.Text("Start", "bold 30px Lato", "#000000");           
+            
+            var graphics = new createjs.Graphics().beginFill("#000000").drawRect(canvas.width/2 -53, canvas.height/2+58, 106, 34);
             var shape = new createjs.Shape(graphics);
 
             graphics = new createjs.Graphics().beginFill("#ffffff").drawRect(canvas.width/2 - 49, canvas.height/2+61, 98, 28);
             var shape2 = new createjs.Shape(graphics);
             
-            text = centerThis(canvas,text);        
+            text = centerThis(canvas,text);
+            text.y = text.y - 20;
             text2 = centerThis(canvas,text2);
-            text2.y = text2.y+75;
+            text2.y = text2.y + 75;
             
             text2.addEventListener("click", function(event) {
                 graphics = new createjs.Graphics().beginFill("#ffffff").drawRect(0, 0, canvas.width, canvas.height);
-                shape3 = new createjs.Shape(graphics);
+                var shape3 = new createjs.Shape(graphics);               
+                var img = new Image();  
+                img.src = "images/yeoman.png"; // image from folder        
+                var loading_img = new createjs.Bitmap(img);                  
+                
                 stage.addChild(shape3);
+                 stage.addChild(loading_img);
                 stage.update(); });
             
             shape.addEventListener("click", function(event) {
                 graphics = new createjs.Graphics().beginFill("#ffffff").drawRect(0, 0, canvas.width, canvas.height);
                 shape3 = new createjs.Shape(graphics);
+                
+                      var img = new Image();  
+                img.src = "images/yeoman.png"; // image from folder        
+                var loading_img = new createjs.Bitmap(img);  
+                
+                
                 stage.addChild(shape3);
+                 stage.addChild(loading_img);
                 stage.update(); });
             
             shape2.addEventListener("click", function(event) {
                  graphics = new createjs.Graphics().beginFill("#ffffff").drawRect(0, 0, canvas.width, canvas.height);
                 shape3 = new createjs.Shape(graphics);
+                
+                var img = new Image();  
+                img.src = "images/yeoman.png"; // image from folder        
+                var loading_img = new createjs.Bitmap(img);  
+               
+                
                 stage.addChild(shape3);
+                 stage.addChild(loading_img);
                 stage.update(); });     
             
             stage.addChild(text);
