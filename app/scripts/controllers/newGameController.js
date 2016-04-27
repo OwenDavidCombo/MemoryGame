4462a9bc4f68750fba044a3134c12c7735bee722
owenmather <owenmather7@gmail.com>
2016-04-27 22:44:55 +0100

    test

--- app/scripts/controllers/newGameController.js
+++ app/scripts/controllers/newGameController.js
@@ -31,7 +31,7 @@
             var stage = new createjs.Stage("demoCanvas");
             
             var text = new createjs.Text("Memory", "bold 120px Lato", "#f8f3f5");
-            var text2 = new createjs.Text("Start", "bold 30px Lato", "#000000");           
+            var text2 = new createjs.Text("start", "bold 30px Lato", "#000000");           
             
             var graphics = new createjs.Graphics().beginFill("#000000").drawRect(canvas.width/2 -53, canvas.height/2+58, 106, 34);
             var shape = new createjs.Shape(graphics);
@@ -44,47 +44,22 @@
             text2 = centerThis(canvas,text2);
             text2.y = text2.y + 75;
             
-            text2.addEventListener("click", function(event) {
+            buttonContainer = new createjs.Container();
+            buttonContainer.addChild(shape,shape2,text2);
+            
+            buttonContainer.addEventListener("click", function(event) {
                 graphics = new createjs.Graphics().beginFill("#ffffff").drawRect(0, 0, canvas.width, canvas.height);
                 var shape3 = new createjs.Shape(graphics);               
                 var img = new Image();  
-                img.src = "images/yeoman.png"; // image from folder        
+                img.src = "/images/yeoman.png"; // image from folder        
                 var loading_img = new createjs.Bitmap(img);                  
                 
                 stage.addChild(shape3);
                  stage.addChild(loading_img);
                 stage.update(); });
             
-            shape.addEventListener("click", function(event) {
-                graphics = new createjs.Graphics().beginFill("#ffffff").drawRect(0, 0, canvas.width, canvas.height);
-                shape3 = new createjs.Shape(graphics);
-                
-                      var img = new Image();  
-                img.src = "images/yeoman.png"; // image from folder        
-                var loading_img = new createjs.Bitmap(img);  
-                
-                
-                stage.addChild(shape3);
-                 stage.addChild(loading_img);
-                stage.update(); });
-            
-            shape2.addEventListener("click", function(event) {
-                 graphics = new createjs.Graphics().beginFill("#ffffff").drawRect(0, 0, canvas.width, canvas.height);
-                shape3 = new createjs.Shape(graphics);
-                
-                var img = new Image();  
-                img.src = "images/yeoman.png"; // image from folder        
-                var loading_img = new createjs.Bitmap(img);  
-               
-                
-                stage.addChild(shape3);
-                 stage.addChild(loading_img);
-                stage.update(); });     
-            
             stage.addChild(text);
-            stage.addChild(shape);
-            stage.addChild(shape2);
-            stage.addChild(text2);
+            stage.addChild(buttonContainer);
             stage.update();
     }
         
